fe9f64438fe31581a94a1c2ee48a14b22b78d5a7
Joe Vu <joe.vu@homeslicesolutions.com>
2014-03-12 16:12:00 -0700

    New file committed

---
+++ backbone-collection-search.js
@@ -0,0 +1,71 @@
+//     Backbone.Collection.search v0.1
+//     by Joe Vu - joe.vu@homeslicesolutions.com
+//     For all details and documentation:
+//     https://github.com/homeslicesolutions/backbone-collection-search
+
+!function(_, Backbone){
+
+  // Extending out
+  _.extend(Backbone.Collection.prototype, {  
+
+    //@ Default Matcher - may be overwritten
+    matcher: function(expected, actual ) {
+      actual = actual.toString().toLowerCase();
+      return actual.indexOf( expected.toLowerCase() ) >= 0;
+    },
+
+    //@ Search function
+    search: function(attributes, keyword, delay) {
+
+      // Variables
+      var that = this,
+          delay = delay || 10;
+
+      // Clear out timeout
+      this._searchTimeout = this._searchTimeout && clearTimeout(this._searchTimeout);
+
+      // Run
+      this._searchTimeout = setTimeout(function(){
+
+        // Instantiate new Collection
+        results = new Backbone.Collection();
+
+        // Iterate through collection models
+        that.each( function( model ){
+
+          // Iterate through each stated attributes
+          _.every( attributes, function( attribute ){
+
+            // Get the attribute value
+            var attrValue = model.get(attribute) && model.get(attribute).toString();
+
+            // Test if found in any attribute add, and on to the next
+            if (attrValue && that.matcher( keyword, attrValue )) {
+              results.add( model );
+              return false;
+            } else {
+              return true;
+            }
+
+          })
+        });
+
+        // Cache the recently searched
+        that._recentlySearched = results;
+
+        // Trigger "search" and return with the new resulted collection
+        that.trigger('search', results );
+
+        // Exit
+        that._searchTimeout = clearTimeout(that._searchTimeout);
+
+      // Append Delay
+      }, delay);
+    },
+
+    //_Cache
+    _recentlySearched: null
+
+  });
+
+}(_, Backbone);

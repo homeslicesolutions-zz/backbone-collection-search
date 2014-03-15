6b8ad9f04dfcae7fa830f30e82a52590c231a00b
Joe Vu <joe.vu@homeslicesolutions.com>
2014-03-15 09:34:40 -0700

    Check for null exception on getSearchQuery

--- backbone-collection-search-ajax.js
+++ backbone-collection-search-ajax.js
@@ -40,7 +40,7 @@
 
     //@ Get recent search query
     getSearchQuery: function() {
-      return this.getSearchResults().getSearchQuery();
+      return this.getSearchResults() && this.getSearchResults().getSearchQuery();
     },
     
     //@ Get recent search results
